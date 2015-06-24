b0680d9c7b8f7794b980561fca151d20d8a30699
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-06-25 01:23:32 +0530

    PEP8 warnings removed

--- mail.py
+++ mail.py
@@ -13,46 +13,47 @@ import os
 
 init()
 
+
 def read_data_from_file():
 
     filename = input('Enter file to read data: ')
 
-    fhan = open( filename, 'r')
+    fhan = open(filename, 'r')
 
     content = fhan.read()
 
     fhan.close()
 
     return content, filename
-    
+
+
 def attach_file():
     base = MIMEBase('application', "octet-stream")
-    
+
     content, filename = read_data_from_file()
-    
+
     base.set_payload(content)
     encoders.encode_base64(base)
     base.add_header('Content-disposition', 'attachment', filename=filename)
 
     return base
 
+
 def prepare_msg():
     msg = MIMEMultipart()
 
-#msg = MIMEText(read_data_from_file())
+    #msg = MIMEText(read_data_from_file())
 
     msg.attach(attach_file())
 
     msg['To'] = input('Enter recipients address: ')
 
-    msg['From'] =  input('Enter login details\nEmail: ') 
+    msg['From'] = input('Enter login details\nEmail: ')
 
     msg['Subject'] = 'Mailing using python'
 
-
-
     return msg
-    
+
 
 def main():
 
@@ -69,7 +70,7 @@ def main():
         msg = prepare_msg()
 
         password = getpass.getpass('Enter password: ')
-    
+
         mail.login(msg['From'], password)
 
         mail.sendmail(msg['From'], msg['To'], msg.as_string())
@@ -77,26 +78,12 @@ def main():
         mail.close()
 
     except ConnectionRefusedError:
-        print(Fore.RED+'Connection refused')
+        print(Fore.RED + 'Connection refused')
     except socket.gaierror:
-        print(Fore.RED+'Problem connecting with host, check hostname or port')
+        print(Fore.RED + 'Problem connecting with host, check hostname or port')
     except smtplib.SMTPAuthenticationError:
-        print(Fore.RED+'Invalide username or password')
+        print(Fore.RED + 'Invalide username or password')
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
