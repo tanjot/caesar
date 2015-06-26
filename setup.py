b3c696e71802c17b373d7763f7a2093fa9d40660
Tanjot Kaur <tanjot28@gmail.com>
2015-06-27 01:15:42 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+add_keywords = dict(
+    entry_points={
+        'console_scripts' : ['caesar=mail.main'],
+    },)
+
+fhan = open('requirements.txt', 'rU')
+requires = [line.strip() for line in fhan.readlines()]
+fhan.close()
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    fhan = open('README.md')
+    long_description = fhan.read()
+    fhan.close()
+
+
+setup(
+    name ='caesar',
+    version='0.1.1',
+    packages=['caesar'],
+    license='GPLv3',
+    description="",
+    long_description=long_description,
+    url='https://github.com/tanjot/caesar',
+    author='Tanjot Kaur',
+    author_email="tanjot28@gmail.com",
+    install_requires=requires,
+    **add_keywords
+)
+
+
+
