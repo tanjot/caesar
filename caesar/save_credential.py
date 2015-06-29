bc9d0b312c1081d44ff4aabd7cc2d3ca7ac4e5f2
Tanjot Kaur <tanjot28@gmail.com>
2015-06-30 00:53:52 +0530

    Change caesar.conf->caesar_user.conf
    Configuration file for server will also be used, hence, create similar names

--- caesar/save_credential.py
+++ caesar/save_credential.py
@@ -2,19 +2,18 @@
 
 from os import path
 
-filename = path.join(path.expanduser('~'), 'caesar.conf')
+cred_filename = path.join(path.expanduser('~'), 'caesar_user.conf')
 
 def save_email_password(email, pwd):
-    with open(filename, 'a') as fhan:
+    with open(cred_filename, 'a') as fhan:
         fhan.write(email+'\n')
         fhan.write(pwd+'\n')
 
 def get_password(email_recv):
     pwd=None
 
-
     try:
-        fhan = open(filename, 'r')
+        fhan = open(cred_filename, 'r')
         email = fhan.readline().strip()
         while email:
             if email == email_recv:
