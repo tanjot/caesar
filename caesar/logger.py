9dc694b8a8924d82d6cb3564a99d19833af4631c
Tanjot Kaur <tanjot28@gmail.com>
2015-07-01 07:54:55 +0530

    Define verbosity levels to use for logging

---
+++ caesar/logger.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import logging
+
+VERBOSITY_LEVELS = {
+                    'debug' : logging.DEBUG,
+                    'error' : logging.ERROR,
+                    'info'  : logging.INFO
+                    }
+
+
