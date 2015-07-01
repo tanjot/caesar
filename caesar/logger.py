03859fb797a1de73a2f9ff3ab73b4a6e21b8724c
Tanjot Kaur <tanjot28@gmail.com>
2015-07-01 07:56:51 +0530

    Create a logging module

--- caesar/logger.py
+++ caesar/logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import logging
+import sys
 
 VERBOSITY_LEVELS = {
                     'debug' : logging.DEBUG,
@@ -8,4 +9,19 @@ VERBOSITY_LEVELS = {
                     'info'  : logging.INFO
                     }
 
+class Logger:
+    def __init__(self):
+        verbosity = VERBOSITY_LEVELS['info']
 
+        log_format = logging.Formatter('%(message)s')
+
+        stream_handle = logging.StreamHandler(sys.stdout)
+        stream_handle.setFormatter(log_format)
+
+        self.log_handle = logging.getLogger(__name__)
+        self.log_handle.setLevel(verbosity)
+
+        self.log_handle.addHandler(stream_handle)
+
+    def print_log(self, msg, verbosity=VERBOSITY_LEVELS['info']):
+        self.log_handle.log(verbosity, msg)
