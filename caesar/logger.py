d4c0716e151974c080015fb4908f6f32a270df31
Tanjot Kaur <tanjot28@gmail.com>
2015-07-01 08:32:01 +0530

    Show colorful logs functionality

--- caesar/logger.py
+++ caesar/logger.py
@@ -3,8 +3,13 @@
 import logging
 import sys
 
+from colorama import init
+from colorama import Fore
+
+init(autoreset=True)
+
 VERBOSITY_LEVELS = {
-                    'debug' : logging.DEBUG,
+                    'debug' : logging.CRITICAL,
                     'error' : logging.ERROR,
                     'info'  : logging.INFO
                     }
@@ -24,4 +29,11 @@ class Logger:
         self.log_handle.addHandler(stream_handle)
 
     def print_log(self, msg, verbosity=VERBOSITY_LEVELS['info']):
-        self.log_handle.log(verbosity, msg)
+        if verbosity == VERBOSITY_LEVELS['debug']:
+            self.log_handle.log(verbosity, Fore.CYAN+msg)
+
+        elif verbosity == VERBOSITY_LEVELS['error']:
+            self.log_handle.log(verbosity, Fore.RED+msg)
+
+        else:
+            self.log_handle.log(verbosity, Fore.MAGENTA+msg)
