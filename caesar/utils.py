b3e20baaaa65eb3ef411b3872bb66a91b4065d67
Tanjot Kaur <tanjot28@gmail.com>
2015-08-02 21:05:40 +0530

    Add function to write a key and value in a file
    The key would be written first in a fresh line while the value would be written in next
    line

--- caesar/utils.py
+++ caesar/utils.py
@@ -13,3 +13,10 @@ def read_dict_from_file(filename):
     with open(filename, 'r') as fhan:
         server_ip = fhan.readline().strip()
         data_in_dict[server_ip] = fhan.readline().strip()
+
+
+def write_dict_to_file(filename, key, value):
+    with open(filename, 'a') as fhan:
+        fhan.write(key+'\n')
+        fhan.write(value+'\n')
+
